6ce25d36fc64f1441e45dfbd9cd7ba164f36747b
José María Escartín Esteban <escartin@irsamc.ups-tlse.fr>
2013-04-05 17:10:21 +0200

    Include define.mk from makefile.

---
+++ code/source_f90/define.mk
@@ -0,0 +1,65 @@
+# Choose compilation parameters
+
+# CF90: Fortran compiler
+# Available options:
+#   * GFORT (serial version only)
+#   * IFORT (serial & parallel & debug version)
+#   * XLF_R (serial & parallel version)
+#   * MPIF90 (parallel version only)
+CF90 = IFORT
+
+# USE_MPI: Variable for parallel processing (only for some compilers)
+# Available options:
+#   * YES
+#   * NO
+USE_MPI = NO
+
+# TYPE_FFT: FFT solver
+# Available options:
+#   * NETLIB
+#   * FFTW
+#   * MKL
+TYPE_FFT = FFTW
+
+# DEBUG: enable debugging
+# Available options:
+#   * YES
+#   * NO
+DEBUG  = NO
+
+# LINK_STATIC: select static linkage of the binary
+# Available options:
+#   * YES
+#   * NO
+LINK_STATIC = YES
+
+# OMP: Invoke OpenMP
+# Available options:
+#   * YES
+#   * NO
+OMP = NO
+
+# MKL_THREADS: Enable MKL threading (only used for TYPE_FFT = MKL)
+# Available options:
+#   * YES
+#   * NO
+MKL_THREADS = YES
+
+# MKL_WRAPPERS: Path to the MKL FFTW wrappers (only used for TYPE_FFT = MKL)
+# Default value:
+MKL_WRAPPERS =
+
+# MKLPATH: Path to the MKL libraries (only used for TYPE_FFT = MKL)
+# Default value:
+MKLPATH =
+# Path for hyperion:
+# MKLPATH = /divers/intel/Compiler/12.1/mkl/lib/intel64
+
+# MKLINCLUDE: Include path for the MKL (only used for TYPE_FFT = MKL)
+# Default value:
+MKLINCLUDE =
+
+# For large boxe sizes, try option
+# '-i-dynamic -mcmodel=medium' or '-shared-intel -mcmodel=medium'
+
+
