04520fc3c1663bdf20983776386e19609dbec32b
Francis Chavanon <francis.chavanon@univ-lemans.fr>
2015-11-23 11:54:17 +0100

    Add licence header

--- code/source_f90/carlo.F90
+++ code/source_f90/carlo.F90
@@ -1,3 +1,21 @@
+!This file is a part of PW-TELEMAN project.
+!PW-TELEMAN is a Time-Dependent Electronic Dynamics in Molecules And Nanosystems library.
+!Copyright (C) 2011-2015  Paul-Gerhard Reinhard, Eric Suraud, Florent Calvayrac,
+!Phuong Mai Dinh, David Brusson, Philipp Wopperer, José María Escartín Esteban.
+!
+!PW-Teleman is free software: you can redistribute it and/or modify
+!it under the terms of the GNU General Public License as published by
+!the Free Software Foundation, either version 3 of the License, or
+!(at your option) any later version.
+!
+!PW-Teleman is distributed in the hope that it will be useful,
+!but WITHOUT ANY WARRANTY; without even the implied warranty of
+!MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!GNU General Public License for more details.
+!
+!You should have received a copy of the GNU General Public License
+!along with PW-Teleman.  If not, see <http://www.gnu.org/licenses/>.
+
 #include"define.h"
  
 
