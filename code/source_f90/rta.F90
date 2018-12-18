1a45f0a61209ae805f9205cff68ca8942e070b02
Paul-Gerhard Reinhard <paul-gerhard.reinhard@fau.de>
2018-12-18 14:10:34 +0100

    temporarily testprint of currents

--- code/source_f90/rta.F90
+++ code/source_f90/rta.F90
@@ -259,6 +259,7 @@ USE params, only: DP, kdfull2,kstate,nstate,dvol,ispin,ifsicp,outnam,&
                   amoy,energy,rtamu,rtamuj,rtasumvar2max,&
                   occup,eqstnspup,eqstnspdw,rtatempinit
 USE kinetic
+USE util, ONLY: prifld
 IMPLICIT NONE
 COMPLEX(DP), INTENT(IN OUT)  :: psi(kdfull2,kstate),psi1(kdfull2,kstate)
 REAL(DP), INTENT(IN OUT)     :: aloc(2*kdfull2),rho(2*kdfull2),occuporth(kstate)
@@ -289,6 +290,9 @@ err=1.d3
  CALL calc_current(curr0,psi)
  curr1=curr0
  ma0=dvol*sum(rhotot0)
+ CALL prifld(curr0(:,1),'j_1')
+ CALL prifld(curr0(:,2),'j_2')
+ CALL prifld(curr0(:,3),'j_3')
 WRITE(*,*) '  j      centfx       err           errj          mu        muj           &
             ma0           ma1'
 j=0
@@ -308,6 +312,8 @@ CALL coul_mfield(rho)
 !at next fermi the density is changed a lot and err and sumvar are exploding. Also energy is ver out of balance
 !in order to control this this version introduces an additionnalloop, with little authority (0.001 on Temp)
 ! and evry tenth step at most, in order to reorder the Occ vs Esp     
+   WRITE(17,'(a,i5/a)') 'RTA protocaol at iterat=',iterat,&
+    '  j  err   errj    diffrho  sumj0 sumj1  diffE  sumvar2 '
    DO while(sumvar2>rtasumvar2max.or.abs(EspTotRef-EspTotAchieved)>1.d-4.or.err>0.2d0)!main loop
         IF((sumvar2<1.d-1.and.err<1.0).and.mod(j,10).eq.0)then !update the occupation numbers without changing the target energy
                 !idea: keep the occ in line with  the energies
@@ -345,8 +351,11 @@ CALL coul_mfield(rho)
       WRITE(*,'(a,i6,20f14.7)') ' RTA-iteration:',j,centfx,err,errj,mu,muj,ma0,ma1,EspTotRef,EspTotAchieved&
       ,EspTotRef-EspTotAchieved,sumvar2,EspPerSpinAchieved(1)-EspPerSpinAchieved(2)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
      CALL flush(6)
+     WRITE(17,'(a,i6,20(1pg14.6))') ' RTA-iteration:',j,err,errj,ma0-ma1,&
+      sum(abs(curr0)),sum(abs(curr1)),EspTotRef-EspTotAchieved,sumvar2
+     CALL flush(17) 
   ENDIF 
-!  IF(j>3) STOP
+  IF(j>100) STOP
 ENDDO!DO j=1...
 OPEN(1000,STATUS='unknown',POSITION='append',FORM='formatted',FILE='pspeed.'//outnam)
         WRITE (1000,'(6g14.5)') (rhotot0(ny*(nyf+nxyf)+ii,1),&
@@ -583,9 +592,9 @@ ENDDO
 
 !do the calc     !?? PGR: is 'c_project' the right thing to call ??
 DO N=1,nstate
-!    CALL cproject(hpsi(:,N),qout,ispin(N),psi1)
-    ovl=wfovlp(psi1(:,N),hpsi(:,N))
-    qout=hpsi(:,N)-psi1(:,N)*ovl
+    CALL cproject(hpsi(:,N),qout,ispin(N),psi1)
+!    ovl=wfovlp(psi1(:,N),hpsi(:,N))
+!    qout=hpsi(:,N)-psi1(:,N)*ovl
     evarsp2 =  SQRT( real( wfovlp(qout,qout) ) )
     sumvar2   = sumvar2 + occup(N)*evarsp2**2
 !WRITE(*,'(a,2i4,4(1pg13.5))') "RTA CALC_VAR:",N,ispin(N),occup(N),evarsp2,&
